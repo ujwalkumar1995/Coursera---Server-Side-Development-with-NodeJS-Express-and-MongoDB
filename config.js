300fa5ce540589b49f72d4787eb6b8574d02d1b5
Ujwal Kumar <ujwalkumar95@gmail.com>
2019-08-04 17:44:04 +0530

    Week 4 Complete

--- config.js
+++ config.js
@@ -2,3 +2,12 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion'
 }
+
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '716649818793024',
+        clientSecret: '131218d51212ad0dd9dc305a98603ed4'
+    }
+}
