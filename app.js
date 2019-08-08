5c8a346a9fb8a29aaf4f35aa42fb1e9b4dccbe1b
Ujwal Kumar <ujwalkumar95@gmail.com>
2019-08-08 12:02:26 +0530

    Added Server Integration with Client

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var uploadRouter = require('./routes/uploadRouter');
 var favoriteRouter = require('./routes/favoriteRouter');
+var commentRouter = require('./routes/commentRouter');
 var config = require('./config');
 const url = config.mongoUrl;
 const mongoose = require('mongoose');
@@ -63,6 +64,7 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
 app.use('/favorites', favoriteRouter);
+app.use('/comments', commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:3003','http://localhost:3004']
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
